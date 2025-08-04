b9b8ef8332c11fbaff7a37012e5d6197734e581d
bourams <brahim.benmansour@outlook.fr>
2025-08-04 10:26:13 +0200

    add tests

---
+++ Mediaspot.IntegrationTests/Fixtures/InMemoryWebApplicationDb.cs
@@ -0,0 +1,30 @@
+﻿using Mediaspot.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Mediaspot.IntegrationTests.Fixtures;
+
+public sealed class InMemoryDbWebApplication : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(Microsoft.AspNetCore.Hosting.IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.FirstOrDefault(d => d.ServiceType.Name.Contains("DbContextOptions"));
+            if (descriptor != null)
+                services.Remove(descriptor);
+
+            services.AddDbContext<MediaspotDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("TestDb_" + Guid.NewGuid());
+            });
+
+            var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<MediaspotDbContext>();
+            db.Database.EnsureCreated();
+        });
+    }
+}
---
+++ tests/Mediaspot.UnitTests/Application/Titles/CreateTitleHandlerTests.cs
@@ -0,0 +1,52 @@
+﻿using Mediaspot.Application.Common;
+using Mediaspot.Application.Titles.Commands;
+using Mediaspot.Domain.Titles;
+using Moq;
+
+namespace Mediaspot.UnitTests.Application.Titles;
+
+public sealed class CreateTitleHandlerTests
+{
+    private readonly Mock<ITitleRepository> _titleRepo;
+    private readonly Mock<IUnitOfWork> _uow;
+    private readonly CreateTitleHandler _sut;
+
+    public CreateTitleHandlerTests()
+    {
+        _titleRepo = new Mock<ITitleRepository>();
+        _uow = new Mock<IUnitOfWork>();
+        _sut = new CreateTitleHandler(_titleRepo.Object, _uow.Object);
+
+    }
+    //Todo: Helper, bogus
+
+    [Fact]
+    public async Task Handle_Should_Call_Persistance_Methods()
+    {
+        // Arrange
+        var titleName = "Unique Title";
+        var description = "Some description";
+        var releaseDate = new DateOnly(2023, 1, 1);
+        var type = TitleType.Movie;
+
+        var cmd = new CreateTitleCommand(titleName, description, releaseDate, type);
+
+        _titleRepo.Setup(r => r.ExistsByNameAsync(titleName, It.IsAny<CancellationToken>()))
+                 .ReturnsAsync(false);
+
+        _titleRepo.Setup(r => r.AddAsync(It.IsAny<Title>(), It.IsAny<CancellationToken>()))
+                  .Returns(Task.CompletedTask);
+
+        _uow.Setup(u => u.SaveChangesAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(1);
+
+        // Act
+        var resultId = await _sut.Handle(cmd, CancellationToken.None);
+
+        //Assert
+        _titleRepo.Verify(r => r.ExistsByNameAsync(titleName, It.IsAny<CancellationToken>()), Times.Once);
+        _titleRepo.Verify(r => r.AddAsync(It.IsAny<Title>(), It.IsAny<CancellationToken>()), Times.Once);
+        _uow.Verify(u => u.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+
+    }
+}
