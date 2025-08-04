ff4b8ef8c2e2feaf6f79a77e75c5f23c09be7947
bourams <brahim.benmansour@outlook.fr>
2025-08-04 10:02:40 +0200

    Add title aggregate root

---
+++ src/Mediaspot.Domain/Titles/Title.cs
@@ -0,0 +1,27 @@
+﻿using Mediaspot.Domain.Common;
+using Mediaspot.Domain.Titles.Events;
+
+namespace Mediaspot.Domain.Titles;
+
+public enum TitleType { Movie = 1, Serie = 2 }
+
+public sealed class Title : AggregateRoot
+{
+    public string Name { get; init; }
+
+    public string? Description { get; init; }
+
+    public DateOnly? ReleaseDate { get; init; }
+
+    public TitleType Type { get; init; }
+
+    public Title(string name, string? description, DateOnly? releaseDate, TitleType type)
+    {
+        Name = name;
+        Description = description;
+        ReleaseDate = releaseDate;
+        Type = type;
+        Raise(new TitleCreated(Id));
+    }
+}
+
